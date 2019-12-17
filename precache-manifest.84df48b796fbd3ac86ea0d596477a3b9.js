14ac8f24d61ecf5b48cbb0e64286393d373b6bc3
pi-da <jpcox@byui.edu>
2019-12-17 13:19:58 -0700

    Updates

---
+++ precache-manifest.84df48b796fbd3ac86ea0d596477a3b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db8b9a77a059f127eca6262776adeb2",
+    "url": "/raspy/index.html"
+  },
+  {
+    "revision": "2578af04d9411053a7ec",
+    "url": "/raspy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1bf11137f9bc8a72453",
+    "url": "/raspy/static/js/2.c4790eff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/raspy/static/js/2.c4790eff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2578af04d9411053a7ec",
+    "url": "/raspy/static/js/main.b28037b8.chunk.js"
+  },
+  {
+    "revision": "e46c50d2960c0bcea9bd",
+    "url": "/raspy/static/js/runtime-main.41631cb7.js"
+  },
+  {
+    "revision": "8f5267b10569aeb65c4fd9e5715a0f1a",
+    "url": "/raspy/static/media/Pie Dark.8f5267b1.png"
+  },
+  {
+    "revision": "1edff8ff76120d38f65cac98c0d4ceb2",
+    "url": "/raspy/static/media/Trend Dark.1edff8ff.png"
+  }
+]);
